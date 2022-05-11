a76f65ea275deb84d63096b448e785e0fc4c6c97
kevinwiranata <kevin.a.wiranata@gmail.com>
2022-05-11 00:43:20 -0700

    update script to match new contracts

--- scripts/transaction.js
+++ scripts/transaction.js
@@ -1,44 +1,47 @@
+require('dotenv').config()
 const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
+const csv = require("fast-csv");
 
-const data = {
-	EV1: {EVID: "ID1", CSOID: "CSO1", PowerFlow: "24.8", RecentMoney: "2.43", Temperature: 45.2, SoC: 87.2,},
-	EV2: {EVID: "ID2", CSOID: "CSO1", PowerFlow: "24.8",RecentMoney: "2.43",Temperature: 45.2,SoC: 87.2,},
-	EV3: {EVID: "ID3",CSOID: "CSO1",PowerFlow: "24.8",RecentMoney: "2.43",Temperature: 45.2,SoC: 87.2},
-	EV4: {EVID: "ID4",CSOID: "CSO1",PowerFlow: "24.8",RecentMoney: "2.43",Temperature: 45.2,SoC: 87.2},
-	EV5: {EVID: "ID5",CSOID: "CSO1",PowerFlow: "24.8",RecentMoney: "2.43",Temperature: 45.2,SoC: 87.2},
-	EV6: {EVID: "ID6",CSOID: "CSO1",PowerFlow: "24.8",RecentMoney: "2.43",Temperature: 45.2,SoC: 87.2},
-};
-
-
+data = [];
+fs.createReadStream(path.resolve(__dirname, "../data", "V2G_one_week.csv"))
+	.pipe(csv.parse({ headers: true }))
+	.on("error", (error) => console.error(error))
+	.on("data", (row) => data.push(row))
+	.on("end", () => sendTx(data));
 
 const sendTx = async (data) => {
 	for (const EV in data) {
 		await axios
 			.post(
-				"https://u0ekz2lj7j-u0xeyovsc6-connect.us0-aws-ws.kaleido.io/transactions?fly-sync=false",
+				"https://u0ekz2lj7j-u0xeyovsc6-connect.us0-aws-ws.kaleido.io/transactions?fly-sync=true",
 				{
 					headers: {
 						type: "SendTransaction",
 						signer: "user2",
 						channel: "default-channel",
-						chaincode: "ev_contract",
+						chaincode: "cso_contract",
 					},
-					func: "UpdateEVData",
+					func: "TransactEnergy",
 					args: [
+						String(data[EV].CSOID),
 						String(data[EV].EVID),
-						"CSO1",
-						String(data[EV].PowerFlow),
-						String(data[EV].RecentMoney),
-						String(data[EV].Temperature),
+						String(data[EV].ChID),
+						String(data[EV].Power_flow),
+						String(data[EV].Money),
+						String(data[EV].Temp),
 						String(data[EV].SoC),
+						String(data[EV].SoH),
 					],
 					init: false,
-				}, {
-					// firefly basic auth 
+				},
+				{
+					// firefly basic auth
 					auth: {
-						username: process.env.username,
-						password: process.env.password
-					}
+						username: process.env.USERNAME,
+						password: process.env.PASSWORD,
+					},
 				}
 			)
 			.then((resp) => {
@@ -50,5 +53,3 @@ const sendTx = async (data) => {
 			});
 	}
 };
-
-sendTx(data);
